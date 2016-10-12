d9a20f0eafa60ef81486bfa1b5584d761181a018
Carlos Saavedra <ca.saavedra@uniandes.edu.co>
2016-10-11 23:44:25 -0500

    Paso 4: Test login (green)

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -1,5 +1,8 @@
 from unittest import TestCase
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class FunctionalTest (TestCase):
     def setUp(self):
@@ -13,13 +16,19 @@ class FunctionalTest (TestCase):
         self.browser.get('http://localhost:8000')
         link = self.browser.find_element_by_id('id_login')
         link.click()
-        usuario = self.browser.find_element_by_id('id_username')
-        usuario.send_keys('admin')
-        pswd = self.browser.find_element_by_id('id_password')
+        wait = WebDriverWait(self.browser, 10)
+
+        user = wait.until(EC.visibility_of_element_located((By.NAME, "username")))
+        user.clear()
+        user.send_keys('admin')
+
+        pswd = wait.until(EC.visibility_of_element_located((By.NAME, "password")))
+        pswd.clear()
         pswd.send_keys('admin1234')
+
         botonLogin = self.browser.find_element_by_id('id_button_login')
         botonLogin.click()
-        self.browser.implicity_wait(5)
+        self.browser.implicitly_wait(5)
         h3 = self.browser.find_element(By.XPATH, '//h3[text()="Administrar"]')
         self.assertIn('Administrar', h3.text)
 
