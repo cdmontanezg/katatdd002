e0bb95886de8aa15c0e73687074fc411cb726484
Carlos Saavedra <ca.saavedra@uniandes.edu.co>
2016-10-11 23:10:34 -0500

    Paso 4: Test login (red)

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -9,4 +9,18 @@ class FunctionalTest (TestCase):
     def test_title (self):
         self.browser.get('http://localhost:8000')
         self.assertIn('Busco Ayuda',self.browser.title)
+    def test_login(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_login')
+        link.click()
+        usuario = self.browser.find_element_by_id('id_user')
+        usuario.send_keys('admin')
+        pswd = self.browser.find_element_by_id('id_pswd')
+        pswd.send_keys('admin1234')
+        botonLogin = self.browser.find_element_by_id('id_button_login')
+        botonLogin.click()
+        self.browser.implicity_wait(5)
+        h3 = self.browser.find_element(By.XPATH, '//h3[text()="Administrar"]')
+        self.assertIn('Administrar', h3.text)
+
 
